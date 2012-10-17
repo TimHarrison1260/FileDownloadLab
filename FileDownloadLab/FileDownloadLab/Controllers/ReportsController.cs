3d9da1f44eeff37adbfad674a57c4514f4d9fe09
Tim Harrison <TimHarrison1260@gmail.com>
2012-10-17 17:30:36 +0100

    Answer to Exercise 1

---
+++ FileDownloadLab/FileDownloadLab/Controllers/ReportsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FileDownloadLab.Models;
+
+namespace FileDownloadLab.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly FileList list = new FileList();
+
+
+        //
+        // GET: /Reports/
+
+        public ActionResult Index()
+        {
+            var results = (from f in list.GetFiles()
+                           select f);
+            return View(results);
+        }
+
+
+        public ActionResult Download(int id)
+        {
+            FileData file = (from f in list.GetFiles()
+                          where f.FileId == id
+                          select f).First();
+            string contentType = "application/pdf";
+            string nameOnTargetMachine = file.FileName;
+            return File(file.FilePath, contentType, nameOnTargetMachine);
+        }
+
+    }
+}
---
+++ FileDownloadLab/FileDownloadLab/Models/FileList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Hosting;
+
+namespace FileDownloadLab.Models
+{
+    public class FileList
+    {
+        public  List<FileData> GetFiles()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(HostingEnvironment.MapPath("~/Files"));
+
+            int id = 0;
+            List<FileData> results = new List<FileData>();
+
+            foreach (var fileInfo in dirInfo.GetFiles())
+            {
+                FileData fileData = new FileData();
+                fileData.FileId = ++id;
+                fileData.FileName = fileInfo.Name;
+                fileData.FilePath = fileInfo.FullName;
+                results.Add(fileData);
+            }
+
+            return results;
+        }
+    }
+}
